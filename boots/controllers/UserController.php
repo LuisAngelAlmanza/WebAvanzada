c1f1e75467d3d5c0119108c088a9e4aff8fddbcc
LuisAngelAlmanza <69695180+LuisAngelAlmanza@users.noreply.github.com>
2020-10-14 20:37:51 -0600

    Add files via upload

--- boots/controllers/UserController.php
+++ boots/controllers/UserController.php
@@ -4,31 +4,46 @@
 	include_once "connectionController.php";
 
 	if (isset($_POST) && isset($_POST['action'])) {
+		if ($_POST['token'] == $_SESSION['token']) {
+			
+			$userController = new UserController();
 
-		$userController = new UserController();
+			switch ($_POST['action']) {
+				case 'store':
+					
+					$name = strip_tags($_POST['name']);
+					$email = strip_tags($_POST['email']);
+					$pass = strip_tags($_POST['password']);
 
-		switch ($_POST['action']) {
-			case 'store':
-				
-				$name = strip_tags($_POST['name']);
-				$email = strip_tags($_POST['email']);
-				$pass = strip_tags($_POST['password']);
-
-				$userController->store($name,$email,$pass);
-
-				break;
-
-			case 'update':
-				
-				$name = strip_tags($_POST['name']);
-				$email = strip_tags($_POST['email']);
-				$pass = strip_tags($_POST['password']);
-				$id = strip_tags($_POST['id']);
-
-				$userController->update($name,$email,$pass,$id);
+					$userController->store($name,$email,$pass);
 
 				break;
+
+				case 'update':
+					$name = strip_tags($_POST['name']);
+					$email = strip_tags($_POST['email']);
+					$pass = strip_tags($_POST['password']);
+					$id = strip_tags($_POST['id']);
+
+					$userController->update($name,$email,$pass,$id);
+				break;
+
+				case 'remove':
+					$id = strip_tags($_POST['user_id']);
+					echo json_encode($userController->remove($id));	
+				break;
+			}
+
+		}else{
+			$respuesta = array(
+				'status' => "error",
+				'message' => "Sin autorización"
+			);
+			echo json_encode($respuesta);
 		}
+		
+
+		
 	}
 
 	Class UserController
@@ -134,6 +149,55 @@
 				$_SESSION['message'] = "Error durante la conexión";
 
 				header('location: ' . $_SERVER['HTTP_REFERER']);
+			}
+		}
+
+		public function remove($id){
+
+			$conn = connect();
+			if (!$conn->connect_error) {
+
+				if($id!=""){
+
+					$query = "delete from users where id = ?";
+
+					$prepared_query = $conn->prepare($query);
+					$prepared_query->bind_param('i', $id);
+
+					if ($prepared_query->execute()){
+
+						$respuesta = array(
+							'status' => "success",
+							"id" => $id,
+							'message' => "el registro se ha eliminado correctamente"
+						);
+						return $respuesta;
+
+					}else{
+
+						$respuesta = array(
+							'status' => "error",
+							'message' => "el registro no se ha eliminado"
+						);
+						return $respuesta;
+					}
+
+				}else{
+
+					$respuesta = array(
+						'status' => "error",
+						'message' => "Verifique la informacion enviada"
+					);
+					return $respuesta;
+				}
+				
+			}else{
+
+				$respuesta = array(
+					'status' => "error",
+					'message' => "el registro no se ha eliminado"
+				);
+				return $respuesta;
 			}
 		}
 	}
